734bc2d87e436989adbe995a72e1e57cfb717026
BananaEmperor <jackson123124@gmail.com>
2020-10-28 15:14:15 -0700

    finished

--- app.py
+++ app.py
@@ -1,5 +1,34 @@
 from flask import Flask, request, render_template, jsonify
-from ShortestPath import *
+import pandas as pd
+import networkx as nx
+import numpy as np
+from networkx.readwrite import json_graph
+import json
+
+#with open('data1.json') as json_file:
+#    data = json.load(json_file)
+
+#G = json_graph.node_link_graph(data)
+
+edges = pd.read_csv('edges.csv')
+nodes = pd.read_csv('nodes.csv')
+
+G = nx.DiGraph()
+for i in nodes['osmid']:
+    G.add_node(i)
+for i in range(len(edges)):
+    G.add_edge(edges.iloc[i]['u'], edges.iloc[i]['v'], weight = edges.iloc[i]['length'])
+
+def get_all_shortest_paths(node_id):
+    return nx.single_source_dijkstra(G, id)
+
+def get_shortest_path_to(start, end):
+    path = nx.dijkstra_path(G, start, end, 'weight')
+    cost = 0
+    for i in range(len(path) - 1):
+        cost += G[path[i]][path[i+1]]['weight']
+    return (path, cost)
+
 
 app = Flask(__name__)
 
@@ -21,7 +50,6 @@ def my_form_post():
     combine = do_something(text1,text2)
     result = {"output": combine}
     result = {str(key): value for key, value in result.items()}
-    print(combine)
     return jsonify(result=result)
 
 if __name__ == '__main__':
